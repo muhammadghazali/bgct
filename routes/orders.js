caac57a402260cd0abe6a06b55870b3228e0766c
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-02-18 03:53:03 +0700

    scaffold initial endpoints to handler order-related operations

--- routes/orders.js
+++ routes/orders.js
@@ -2,10 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(db) {
-  /* GET orders listing. */
-  router.get('/', function(req, res, next) {
-    console.log('db');
+  router.post('/', function(req, res, next) {
     res.send('respond with a resource');
+  });
+
+  router.delete('/', function(req, res, next) {
+    res.send('respond with a resource');
+  });
+
+  router.get('/', function(req, res, next) {
+    // TODO handle `Show all orders to a particular address` query
+    // TODO handle `Show all orders to a particular address` query
+    // TODO handle `Display how often each item has been ordered,
+    //  in descending order (ie in the above example, 2x for Macbook and Inline
+    // skates, 1x for the rest)` query
+    res.send('show all orders');
   });
 
   return router;
