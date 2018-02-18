cb1bd7478ffc99154d396ac7ebb744479f7094f9
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-02-19 01:24:36 +0700

    remove dead spec

--- test/unit/routes/orders/list-order.spec.js
+++ test/unit/routes/orders/list-order.spec.js
@@ -8,38 +8,6 @@ describe('Test the list order route handler', function() {
     expect(listOrdersRouteHandler).to.be.a('function');
   });
 
-  describe('Test `Show all orders to a particular address`', function() {
-    it('should proceed the request if the address is provided in the query', function() {
-      const req = {
-        query: {
-          address: 'Steindamm 80'
-        }
-      };
-      const res = new MockExpressResponse();
-      const spyStatus = sinon.spy(res, 'status').withArgs(200);
-
-      listOrdersRouteHandler(req, res);
-
-      expect(spyStatus.calledOnce).to.be.true;
-    });
-
-    it('should not proceed the request if the address is empty', function() {
-      const req = {
-        query: {
-          address: ''
-        }
-      };
-      const res = new MockExpressResponse();
-      const spySendResponse = sinon.spy(res, 'send').withArgs({
-        message: 'The order address should not be empty'
-      });
-
-      listOrdersRouteHandler(req, res);
-
-      expect(spySendResponse.calledOnce).to.be.true;
-    });
-  });
-
   describe('Test `Display how often each item has been ordered,`', function() {
     it('should proceed the request if the count is provided in the query', function() {
       const req = {
